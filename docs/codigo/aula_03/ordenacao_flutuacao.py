184102f71f5a976892ae5a905a2040aa956f54bf
Everson Otoni <everson.ott@gmail.com>
2024-08-13 12:12:25 -0300

    Reformulação da contagem das linhas e apresentação mais dididática da iteração

--- docs/codigo/aula_03/ordenacao_flutuacao.py
+++ docs/codigo/aula_03/ordenacao_flutuacao.py
@@ -1,20 +1,21 @@
 def ordenacao_flutuacao_demonstrativo(arranjo):
     contador = 0
-    for i in range(len(arranjo)):
+    contador2 = 0
+    for i in range(len(arranjo) - 1):
         print("\ni: ", i)
         for j in range(len(arranjo) - i - 1):
             print("j: ", j, end="")
             if arranjo[j] > arranjo[j + 1]:
                 arranjo[j], arranjo[j + 1] = arranjo[j + 1], arranjo[j]
                 contador += 1
-            print(" -> ", arranjo)
-        contador += 1
+                contador2 += 1
+                print(" -> trocou", arranjo[j+1], "por", arranjo[j], "e vice-versa.")
+                print("->", arranjo)
+        contador2 += 1
 
-    arranjo2 = [55, 23, 12, 2]
-    ordenacao_flutuacao(arranjo2)
-    print(arranjo2)
 
-    print(contador)
+    print("\nLinha 2 e 3 foram executadas:", contador)
+    print("Linha 4, 5 e 6 foram executadas:", contador2)
 
 
 def ordenacao_flutuacao(arranjo):
