5f79539c080dd42e71c72b0785983c2c828e1f0c
Everson Otoni <everson.ott@gmail.com>
2024-08-13 11:52:15 -0300

    Exemplo do uso aprimorado de módulos, feito em aula

---
+++ docs/codigo/aula_03/aula_03_ex3.py
@@ -0,0 +1,8 @@
+import aula_03
+
+arranjo_yuri = [1917,1575,1444,1426,1327,1261,1219,1139,1119,755,674,646,535,531,456,440,392,387,192,183]
+
+
+aula_03.ordenacao_flutuacao_demonstrativo(arranjo_yuri)
+
+
